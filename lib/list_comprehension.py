43ee06346f21b23a03c6b80868a01e8f4a534c48
Gloria Birundu <birundugloria@gmail.com>
2025-09-01 01:15:29 +0300

    re-submit codegrade-all tests passing

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,6 +1,6 @@
 def return_evens(num_list):
-    """Return a list of even numbers from the given list."""
-    return [n for n in num_list if n % 2 == 0]
+    return [num for num in num_list if num % 2 == 0]
+
 def make_exclamation(sentence_list):
-    """Return a list of sentences with exclamation marks added."""
     return [sentence + "!" for sentence in sentence_list]
+
